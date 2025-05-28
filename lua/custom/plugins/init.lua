ac0bde422d8d9b529857fc6fce4e93c8affa05bc
JohnRob1 <jrobinson6114@gmail.com>
2025-05-27 20:14:41 -0400

    new custom config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,61 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+-- Custom Options
+vim.g.have_nerd_font = true
+
+vim.o.relativenumber = true
+vim.o.expandtab = true
+
+return {
+  -- Aesthetics
+  {
+    'Darazaki/indent-o-matic',
+  },
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 999,
+    config = function()
+      vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+  {
+    'xiyaowong/transparent.nvim',
+    config = function()
+      require('transparent').clear_prefix 'NeoTree'
+      require('transparent').setup {
+        exclude_groups = {
+          'StatusLine',
+          'StatusLineNC',
+        },
+      }
+    end,
+  },
+
+  -- Tools
+  {
+    'toppair/peek.nvim',
+    event = { 'VeryLazy' },
+    build = 'deno task --quiet build:fast',
+    config = function()
+      require('peek').setup { app = 'browser' }
+      vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    end,
+  },
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = true,
+  },
+  {
+    'kylechui/nvim-surround',
+    version = '^3.0.0', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+}
