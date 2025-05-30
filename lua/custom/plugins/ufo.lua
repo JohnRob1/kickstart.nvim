0cac416eac0e767e0520327ffbb8aa51d9da644e
JohnRob1 <jrobinson6114@gmail.com>
2025-05-30 16:33:43 -0400

    fixed ufo config

--- lua/custom/plugins/ufo.lua
+++ lua/custom/plugins/ufo.lua
@@ -1,9 +1,35 @@
 vim.o.foldlevel = 99
 vim.o.foldenable = true
 vim.o.foldlevelstart = 3
-vim.o.foldcolumn = 'auto:9'
+vim.o.foldcolumn = '0'
 vim.o.foldmethod = 'manual'
-vim.o.fillchars = 'fold: ,foldopen:,foldsep:│,foldclose:'
+
+local function foldTextFormatter(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+  table.insert(newVirtText, { suffix, 'MoreMsg' })
+  return newVirtText
+end
 
 vim.keymap.set('n', '<leader>K', function()
   require('ufo').peekFoldedLinesUnderCursor()
@@ -22,5 +48,7 @@ return {
   dependencies = {
     'kevinhwang91/promise-async',
   },
-  opts = {},
+  opts = {
+    fold_virt_text_handler = foldTextFormatter,
+  },
 }
