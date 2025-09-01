a27f08a5cf27fbba2064613f784093841fae8440
JohnRob1 <jrobinson6114@gmail.com>
2025-09-01 15:47:28 -0400

    move all keymaps, opts, autocommands, aliases to custom/init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,15 @@ vim.g.have_nerd_font = true
 vim.o.relativenumber = true
 vim.o.expandtab = true
 
+vim.keymap.set({ 'n', 'v' }, '<LocalLeader>a', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true })
+vim.keymap.set('v', 'ga', '<cmd>CodeCompanionChat Add<cr>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>')
+vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>')
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd [[cab cc CodeCompanion]]
+
 -- guess-indent is not loaded in main init.lua
 require('guess-indent').setup {}
 
