82019144572aa3b9ebe11b8a32e333eff54197e6
JohnRoMBUU <johnathon.robinson@malibuboats.com>
2025-11-13 21:48:15 -0500

    switch to opencode

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,17 +1,18 @@
 return {
-  {
-    'olimorris/codecompanion.nvim',
-    -- TODO: Use Claude with Copilot when possible
-    opts = {},
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-  },
-  {
-    {
-      'MeanderingProgrammer/render-markdown.nvim',
-      ft = { 'markdown', 'codecompanion' },
-    },
-  },
+  -- {
+  --   'olimorris/codecompanion.nvim',
+  --   -- TODO: Use Claude with Copilot when possible
+  --   opts = {},
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'github/copilot.vim',
+  --   },
+  -- },
+  -- {
+  --   {
+  --     'MeanderingProgrammer/render-markdown.nvim',
+  --     ft = { 'markdown', 'codecompanion' },
+  --   },
+  -- },
 }
