11362a2c2e20f905ff250c26852801c4f5e84eb3
JohnRob1 <robin557@purdue.edu>
2026-01-12 13:28:09 -0500

    copilot-blink.cmp integration

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,9 +1,33 @@
-vim.keymap.set('i', '<C-n>', 'copilot#Accept("\\<CR>")', {
-  expr = true,
-  replace_keycodes = false,
-})
-vim.g.copilot_no_tab_map = true
-
 return {
-  'github/copilot.vim',
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      }
+    end,
+  },
+  {
+    'giuxtaposition/blink-cmp-copilot',
+    dependencies = { 'zbirenbaum/copilot.lua' },
+  },
+  {
+    'saghen/blink.cmp',
+    optional = true,
+    opts = function(_, opts)
+      table.insert(opts.sources.default, 'copilot')
+
+      opts.sources.providers.copilot = {
+        name = 'copilot',
+        module = 'blink-cmp-copilot',
+        score_offset = 100,
+        async = true,
+      }
+
+      return opts
+    end,
+  },
 }
