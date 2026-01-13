4047b35439fa524dad46cb87a8e714d071ea0c86
JohnRoMBUU <johnathon.robinson@malibuboats.com>
2026-01-13 08:54:08 -0500

    no more swapfile

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,7 @@ vim.g.have_nerd_font = true
 
 vim.o.relativenumber = true
 vim.o.expandtab = true
+vim.o.swapfile = false
 
 vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>')
 vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>')
